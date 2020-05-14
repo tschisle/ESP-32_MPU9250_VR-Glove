7b3a2a1110262cfb6edd0d0fd5e616d7d88444c0
Unknown <tschisle@purdue.edu>
2020-05-14 02:03:42 -0400

    forgot to save

--- src/PredictiveLeastSquaresFilter.h
+++ src/PredictiveLeastSquaresFilter.h
@@ -3,14 +3,16 @@
 
 #include <Arduino.h>
 
-void PLSF_Initialization(float ax, float ay, float az, float gx, float gy,
-                              float gz, float mx, float my, float mz,
-                              float deltat);
 
-float PLSF_Update(float ax, float ay, float az, float gx, float gy,
-                              float gz, float mx, float my, float mz,
-                              float deltat);
+class PLSF_Filter
+{
+	_
+	public:
+	void PLSF_Initialization(uint8_t );
+
+	float PLSF_Update(float );
 							  
-void PLSF_Clear();
+	void PLSF_Clear();
+};
 
 #endif 
