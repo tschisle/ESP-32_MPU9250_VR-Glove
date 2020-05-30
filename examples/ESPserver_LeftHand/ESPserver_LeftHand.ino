b2db6c57166045405d680cc16feef22ab4d42317
Unknown <tschisle@purdue.edu>
2020-05-30 04:12:59 -0400

    Added gesture recognition and overhauled touch readings
    Gestures: Swipes (called "slide" in code), hold, press

--- examples/ESPserver_LeftHand/ESPserver_LeftHand.ino
+++ examples/ESPserver_LeftHand/ESPserver_LeftHand.ino
@@ -3,34 +3,226 @@
 //touch input always available and magnetic sensing only available after calibration received from right hand.
 
 /* GOALS:
- * - Replace delay calls with a time dependent call 
- * - Make every Serial call compiled based off a defined symbol
- * - Replace touch on/off elseif's with a more robust system  
- * - Replace touch multiple tolerances with an autocalibrated value 
- * - Replace/implement gesture recognization 
- */
+   - Make every Serial call compiled based off a defined symbol
+   ~ Replace touch multiple tolerances with an autocalibrated value
+*/
 
 // Just test touch pin - Touch0 is T0 which is on GPIO 4
 /* Press, hold, slide - trans number
-   1  - press T7
-   2  - press T6
-   3  - press T8
-   4  - press T9
+   1  - press T7 - B - Right
+   2  - press T6 - A - Bottom
+   3  - press T8 - Y - Top
+   4  - press T9 - X - Left
    5  - swipe from A to B
    6  - swipe from B to A
-   7  - swipe from B to Y
-   8  - swipe from Y to B
-   9  - hold A
-   10 - hold B
-   11 - hold X
-   12 - hold Y
+   7  - swipe from X to Y
+   8  - swipe from Y to X
+   9  - hold B
+   10 - hold A
+   11 - hold Y
+   12 - hold X
 */
 #include <WiFi.h>
 #include <WiFiUdp.h>
 //_____________________________________________________________________________________
 
+class Touchpad
+{
+    unsigned long tpmat[3][2][2]; //layer, row, column
+    /*  touch |top, right|   press |top, right|    time    |top, right|
+        flags |left, bott|,  order |left, bott|,   pressed |left, bott|
+        Y - Top; B - Right; A - Bottom; X - Left
+    */
+    const unsigned long inter_button_slide_time = 250; //number of milliseconds between button presses to distinguish a slide from quick button presses
+    const unsigned long button_hold_time = 500; //number of milliseconds between distinguishing a press from a hold
+    int slide_timer = 0; //counts down time between presses
+    int hold_timer = 0; //counts down time holding a press
+    const int set_point_h = 45; //value based from trial and error
+    const int set_point_l = 10; //value based from trial and error
+    int T9_init = 0;  //4 - Y - Top
+    int T8_init = 0;  //3 - A - Bottom
+    int T7_init = 0;  //1 - B - Right
+    int T6_init = 0;  //2 - X - Left
+    bool touched = false;//gets set when a touch is detected
+    int num_touches = 0; //gets the number of touches within the gesture period
+    unsigned long temp_touch_time_ref = 0;//serves as the reference time for the gesture recognition.
+    bool slide_detected = false;
+    bool hold_detected = false;
+    bool press_detected = false;
+    bool hold_mask[2][2] = {{false, false}, {false, false}}; //prevents presses from being reported after a hold
+    int press_value; //allows the tpmat to be fully cleared
+  public:
+    initialize() {
+      int i, j, k;
+      delay(300); //will likely fix the initial reading issue
+      T9_init = touchRead(T9); //this first reading is wrong - taken again at the end of the readings
+      T8_init = touchRead(T8); //3 - A - Bottom
+      T7_init = touchRead(T7); //1 - B - Right
+      T6_init = touchRead(T6); //2 - X - Left
+      T9_init = touchRead(T9); //4 - Y - Top
+      for (i = 0; i < 3; i++) {
+        for (j = 0; j < 2; j++) {
+          for (k = 0; k < 2; k++) {
+            tpmat[i][j][k] = 0;
+          }
+        }
+      }
+      num_touches = 0;
+      temp_touch_time_ref = 0;
+    }
+
+    int update()
+    {
+      int i, j;
+      slide_detected = false;
+      hold_detected = false;
+      press_detected = false;
+      matrixUpdate();  //also performs touch reads
+      /*NOTES
+         Since there isn't a command for pressing multiple buttons at once the code assumes this won't happen.
+         This can be added in the slide detection.
+
+         Additionally the detection code is written to allow gesture changes with continuous contact to the touchpad
+
+         Possible issue: The hold mask may not be completely robust as it's used now.    
+      */
+      //Slide Detection -- Reports the last slid-over button
+      if (num_touches > 1) {
+        if (temp_touch_time_ref != 0) {
+          press_value = 0;
+          for (j = 0; j < 2; j++) {
+            for (k = 0; k < 2; k++) {
+              if ((num_touches == tpmat[1][j][k]) && (tpmat[1][(j + 1) % 2][(k + 1) % 2] > 0)) {
+                //Organized to remove else's
+                slide_detected = true;
+                press_value = 2; //2 - X - Left
+                if (j == 0) {
+                  press_value = 1; //1 - B - Right
+                }
+                if (j == k) {
+                  press_value = 4; //4 - Y - Top
+                  if (j == 0) {
+                    press_value = 3; //3 - A - Bottom
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      //Hold Detection -- Reports the longest held button
+      if (hold_timer == 0) {
+        press_value = 0;
+        for (j = 0; j < 2; j++) {
+          for (k = 0; k < 2; k++) {
+            if (tpmat[2][j][k] > (button_hold_time - 1)) {
+              //Organized to remove else's
+              hold_detected = true;
+              hold_mask[j][k] = true;
+              press_value = 2; //2 - X - Left
+              if (j == 0) {
+                press_value = 1; //1 - B - Right
+              }
+              if (j == k) {
+                press_value = 4; //4 - Y - Top
+                if (j == 0) {
+                  press_value = 3; //3 - A - Bottom
+                }
+              }
+            }
+          }
+        }
+      }
+      //Press Detection  -- Reports the first pressed button
+      if (!touched && (slide_timer == 0)) {
+        press_value = 0;
+        for (j = 0; j < 2; j++) {
+          for (k = 0; k < 2; k++) {
+            if ((tpmat[1][j][k] == 1) && (!hold_mask[j][k])) {
+              //Organized to remove else's
+              if (!hold_mask[j][k]) {
+                hold_mask[j][k] = false;
+              } else {
+                press_detected = true;
+                press_value = 2; //2 - X - Left
+                if (j == 0) {
+                  press_value = 1; //1 - B - Right
+                }
+                if (j == k) {
+                  press_value = 4; //4 - Y - Top
+                  if (j == 0) {
+                    press_value = 3; //3 - A - Bottom
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      //End Gesture
+      if (slide_detected || hold_detected || press_detected) {
+        num_touches = 0;
+        temp_touch_time_ref = 0;
+        for (j = 0; j < 2; j++) {
+          for (k = 0; k < 2; k++) {
+            tpmat[1][j][k] = 0;
+            tpmat[2][j][k] = 0;
+          }
+        }
+        if (slide_detected) {
+          return (press_value + 4);
+        } else if (hold_detected) {
+          return (press_value + 8);
+        } else if (hold_detected) {
+          return (press_value);
+        } else {
+          return (0);
+        }
+      }
+    }
+
+  private:
+    void matrixUpdate(void)
+    {
+      tpmat[0][0][0] = T8_init - touchRead(T8); //3 - A - Bottom
+      tpmat[0][0][1] = T7_init - touchRead(T7); //1 - B - Right
+      tpmat[0][1][0] = T6_init - touchRead(T6); //2 - X - Left
+      tpmat[0][1][1] = T9_init - touchRead(T9); //4 - Y - Top
+      int j, k;
+      touched = false;
+      if (slide_timer > 0) {
+        slide_timer--;
+      }
+      for (j = 0; j < 2; j++) {
+        for (k = 0; k < 2; k++) {
+          if (tpmat[0][j][k] > ((set_point_h + set_point_l) / 2)) {
+            tpmat[0][j][k] = 1; //boolean TRUE
+            touched = true;
+            num_touches++;
+            slide_hold_timer = (int) inter_button_slide_time;
+            if (hold_timer > 0) {
+              hold_timer--;
+            }
+            tpmat[1][j][k] = num_touches; //Touch Order
+            if (temp_touch_time_ref == 0) {
+              temp_touch_time_ref = millis();
+            } else {
+              tpmat[2][j][k] = millis() - temp_touch_time_ref; //time held relative to the start of the gesture
+            }
+          } else {
+            tpmat[0][j][k] = 0; //boolean FALSE
+          }
+        }
+      }
+      if (!touched) {
+        hold_timer = (int) button_hold_time;
+      }
+    }
+};
+
 //WiFi configuration
 WiFiUDP Udp; // Creation of wifi Udp instance
+Touchpad touch; //Creates touchpad object
 char packetBuffer[255];
 unsigned int localPort = 9999;
 const char *ssid =  "NETGEAR36-5G";// "NETGEAR36-5G";
@@ -43,12 +235,6 @@ IPAddress ipCliente3(192, 168, 1, 8);  //UNITY client ip
 //Touch configuration
 int command = 0;
 int aux_command = 0;
-const int set_point_h = 45; //value based from trial and error
-const int set_point_l = 10; //value based from trial and error
-int T9_init = 0;
-int T8_init = 0;
-int T7_init = 0;
-int T6_init = 0;
 
 
 //_____________________________________________________________________________________
@@ -61,21 +247,7 @@ void setup() {
   //_____________________________________________________________________________________
   //touch sensing setup
   Serial.println("ESP32 Touch Test");
-  //initial reading of the sensors
-  Serial.print("Initial Values  ");
-  T9_init = touchRead(T9); //this first reading is wrong - taken again at the end of the readings
-  T8_init = touchRead(T8);
-  Serial.print("T8 init ");
-  Serial.println(T8_init);
-  T7_init = touchRead(T7);
-  Serial.print("T7 init ");
-  Serial.println(T7_init);
-  T6_init = touchRead(T6);
-  Serial.print("T6 init ");
-  Serial.println(T6_init);
-  T9_init = touchRead(T9);
-  Serial.print("T9 init  ");
-  Serial.println(T9_init);
+  touch.initialize();
   delay(500); // delay to check the values in the serial monitor
 }
 
@@ -102,40 +274,11 @@ void loop() {
     Udp.endPacket();
   }
   //-----------------------------------------------------------------
-  /* NOTES:
-   * Since the tolerance of the touchread conditions for the setting and clearing of the command variable are not the same for both, there exists a state between 
-   * where the variable "command" isn't set or cleared.  This creates a schmitt trigger like performance. This may not be ideal since the tolerances are based on
-   * arbitrary values and actual performance may cause the reading to not reach the tolerance.  Additionally switching to a single tolerance value and utilizing   
-   * an actual switch case structure will simplify code.
-   * The current setup and a switch case structure create a hierarchy which ranks setting and clearing from reading T7 at the top to reading from T9 last.  
-   * This current setup doesn't handle multiple presses and is likely to cause issues, since you can easily accidently press a higher ranked pin/pad which alters
-   * the command variable.  
-   */
-  //Switch Case based on sensor values, but with if else statements
-  if ((T7_init - touchRead(T7) ) > set_point_h) {
-    command = 1; //command[0] = 1; //this is for the wifi part
-  } else if ((T6_init - touchRead(T6)) > set_point_h) {
-    command = 2;//command[0] = 2;
-  } else if ((T8_init - touchRead(T8)) > set_point_h) {
-    command = 3; //command[0] = 3;
-  } else if ((T9_init - touchRead(T9)) > set_point_h) {
-    command = 4; //command[0] = 4;
-  }
+  command = touch.update();
   Serial.print("command  ");
   Serial.println(command);
   sendReadings(command); //sends a messace every cycle to the client 192, 168, 4, 10
   sendReadingsUnity(command); //sends a messace every cycle to the client 192, 168, 4, 12
-  delay(100);// needs to be removed
-  //to make zero afer each touch
-  if ((T7_init - touchRead(T7) ) < set_point_l) {  
-    command = 0; //command[0] = 1; //this is for the wifi part
-  } else if ((T6_init - touchRead(T6)) < set_point_l) {
-    command = 0;//command[0] = 2;
-  } else if ((T8_init - touchRead(T8)) < set_point_l) {
-    command = 0; //command[0] = 3;
-  } else if ((T9_init - touchRead(T9)) < set_point_l) {
-    command = 0; //command[0] = 4;
-  }
   //-----------------------------------------------------------------
 }//end loop
 
